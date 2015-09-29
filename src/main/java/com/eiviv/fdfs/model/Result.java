322e2c883f76dad50718c550cf0c68c93a9d344a
Jrain <doersoul@126.com>
2015-09-29 12:30:34 +0800

    修改提示

--- src/main/java/com/eiviv/fdfs/model/Result.java
+++ src/main/java/com/eiviv/fdfs/model/Result.java
@@ -44,7 +44,7 @@ public class Result<T extends Serializable> {
 			state = "成功";
 			break;
 		case Context.ERR_NO_ENOENT:
-			state = "storage服务器尚未准备好,请检查网络或是否重启storage服务等";
+			state = "storage服务尚未准备好,请检查网络是否异常或者是否正在重启storage服务等";
 			break;
 		case Context.ERR_NO_EIO:
 			state = "IO异常";
